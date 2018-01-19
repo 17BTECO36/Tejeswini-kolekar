01ddb68a5ffbf625b84e2d9231799deb39147784
17BTECO36 <35587951+17BTECO36@users.noreply.github.com>
2018-01-19 10:38:25 +0530

    Add files via upload

---
+++ add.py
@@ -0,0 +1,5 @@
+a=int(input('enter the a value'))
+b= int(input('enter the b value'))
+c=a+b
+print(c)
+
---
+++ program-1.py
@@ -0,0 +1,7 @@
+#my first phyton program
+print("tejeswini")
+print("im studying in cmru")
+print("reading")
+print("100+300")
+print(100+20)
+print(500-350)
