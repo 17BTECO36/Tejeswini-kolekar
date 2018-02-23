656439e959c3613f91a6a1a687289d45b06b63cc
17BTECO36 <35587951+17BTECO36@users.noreply.github.com>
2018-02-23 10:24:51 +0530

    Add files via upload

---
+++ break_con_pro.py
@@ -0,0 +1,8 @@
+for i in (1,2,3,4,5):
+    if(i==3):
+        break
+    print(i)
+print("out of loop")
+print(i)
+    
+    
---
+++ def_program.py
@@ -0,0 +1,16 @@
+def add(d,g):
+    print(d+g)
+    print("sucess")
+def sub(x,y):
+     print("subtraction")
+     return(x-y)
+def mul(r,o):
+    print(r*o)
+    print("multiplication")
+def div(X,Y):
+    print(X/Y)
+
+add(75+90)
+sub(54-5)
+mul(78*7)
+div(4/8)
